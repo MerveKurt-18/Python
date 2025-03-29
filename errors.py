f06f6afd08d1be100f02d70218cbdc2d7343e8ef
MerveKurt <>
2025-03-29 21:28:42 +0300

    Değişiklik mesajı

---
+++ errors.py
@@ -0,0 +1,14 @@
+
+#try:
+    # hata olabilecek blok 
+
+#except:
+    #hata mesajı
+
+try:
+    a = int(input("sayı 1 : "))
+    b = int(input("sayı 2 : "))
+    print(a+b)
+
+except:
+    print("hata oluştu.")
