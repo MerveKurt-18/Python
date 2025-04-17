c761f272362419877042354fbcff99aff87732a5
Merve Kurt <merve.kurt2831@gmail.com>
2025-04-17 20:19:59 +0300

    Değişiklik mesajı

---
+++ math_module.py
@@ -0,0 +1,30 @@
+
+import math
+"""
+sonuc = dir(math)
+print(sonuc)
+
+sonuc1 = help(math.ceil)
+print(sonuc1)
+"""
+sonuc = math.sqrt(36)
+print(sonuc)
+sonuc1 = math.factorial(5)
+print(sonuc1)
+sonuc2 = math.floor(6.8) # aşağı yuvarlama
+print(sonuc2)
+sonuc3 = math.ceil(7.2) # yukarı yuvarlama
+print(sonuc3)
+
+import math as islem
+
+sonuc4 = islem.factorial(6)
+print(sonuc4)
+
+from math import *
+sonuc5 = sqrt(64)
+print(sonuc5)
+sonuc6 = factorial(8)
+print(sonuc6)
+
+from math import sqrt # böyle yaparsak sadece sqrt fonksiyonunu kullanabiliriz.
