03089fbb85753b9c253544aba2d037f848c3608c
Merve Kurt <merve.kurt2831@gmail.com>
2025-05-02 18:54:06 +0300

    ...

---
+++ inheritance.py
@@ -0,0 +1,27 @@
+# Inheritance : Miras alma, Kalıtım
+
+# Ogrenci
+    # numarası, ad,soyad, cinsiyet, adres, sınıf
+
+# Ogretmen
+    # ad, soyad, cinsiyet, adres, brans, maas
+
+# Ogretmen ve Ogrenci kişilerinde ortak olanları Kişi sınıfında topladık.
+
+
+# Kişi (parent class, bas class)
+    # ad, soyad, cinsiyet, adres
+    # yas_hesapla()
+
+# Ogrenci (child class)
+    # numarası
+    # sınıfı
+    # ortalama_alma()
+
+# Ogretmen (child class)
+    # brans
+    # maas
+    # maas_hesapla()
+    # terfi()
+
+
